aac9ca88ca23b988eb0182ae58e64416309c2ef2
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-10-30 10:22:40 +0700

    Percobaan 2

---
+++ Square24.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Square24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int iOuter = 1; iOuter <= n; iOuter++) {
+            for (int i = 1; i <= n; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
